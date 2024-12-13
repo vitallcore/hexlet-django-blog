cfda7909a2fa1217577db617836230033d3e32af
vitallcore <vitaslipknot@gmail.com>
2024-12-14 02:04:09 +0600

    django blog

---
+++ hexlet_django_blog/views.py
@@ -0,0 +1,10 @@
+# hexlet_django_blog/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html', context={
+        'who': 'World',
+    })
+
+def about(request):
+    return render(request, 'about.html')
