5e8694910460fb4c46213bdd6de0d61bfe266e15
vitallcore <vitaslipknot@gmail.com>
2024-12-16 17:08:39 +0600

    django blog

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
-    path("admin/", admin.site.urls),
+    path('articles/', include('hexlet_django_blog.article.urls')),
+    path('admin/', admin.site.urls),
 ]
