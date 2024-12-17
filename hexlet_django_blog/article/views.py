309b019511b9fbd63b64c451cad2146ee510210d
vitallcore <vitaslipknot@gmail.com>
2024-12-18 00:47:04 +0600

    django blog

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,5 @@
-# hexlet_django_blog/article/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('article')
+    app_name = 'hexlet_django_blog.article'
+    return render(request, 'articles/index.html', context={'app_name': app_name})
