b8ab5bd8a99e2aead1159fa4325d674d9c9d6b60
vitallcore <vitaslipknot@gmail.com>
2024-12-18 21:43:45 +0600

    django blog

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,16 @@
 # hexlet_django_blog/views.py
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class HomePageView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
+
 
 def about(request):
     return render(request, 'about.html')
