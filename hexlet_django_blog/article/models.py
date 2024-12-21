b2e30d28d8718bc0e7cead84a3842ea4a150af4c
vitallcore <vitaslipknot@gmail.com>
2024-12-21 21:46:27 +0600

    django blog

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -2,7 +2,10 @@
 from django.db import models
 
 class Article(models.Model):
-    name = models.CharField(max_length=200) # название статьи
-    body = models.TextField() # тело статьи
+    name = models.CharField(max_length=200)
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
